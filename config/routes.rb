df452d49150796dda17c46daea09b2d70028fad2
Lizzie <lizzie.sorin@hotmail.fr>
2018-10-18 07:58:00 -0700

    home page structure built and wired up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+root to: 'application#index'
+get '/home', to: 'application#index', as: 'home'
+
 end
