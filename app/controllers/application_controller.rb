b358449e3c9d923129d47ac5fead0b73da2cb807
Lizzie <lizzie.sorin@hotmail.fr>
2018-10-23 15:54:19 -0700

    display the companies of the partners to look for a contact

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,19 @@ class ApplicationController < ActionController::Base
   	@projects = Project.all
   	@team_mates = TeamMate.all
   	@contacts = Contact.all
+  	@contact_companies = @contacts.select(:company).map(&:company).uniq
+
   	render :home
   end
+
+ #  def contact_companies
+	# 	@contact_companies = []
+	# 	@contacts.each do |contact|
+	# 		if @contact_companies.exclude?(contact.company)
+	# 		contact_companies.push(contact.company)
+	# 		end
+	# 	end
+	# 	return @contact_companies	
+	# end
 
 end
