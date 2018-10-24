0f243da503a4d1e74abad5b7edba3b196461ce99
Lizzie <lizzie.sorin@hotmail.fr>
2018-10-24 13:01:20 -0700

    export the contacts js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
-console.log("console log")
+
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
 // vendor/assets/javascripts directory can be referenced here using a relative path.
 //
@@ -15,80 +15,9 @@ console.log("console log")
 //= require turbolinks
 //= require_tree .
 
-$(function() {
-    attachListeners();
-});
 
 
-function attachListeners() {
-    $(".clients").on("click", openClients);     
-    $(".contacts").on("click", openContacts);
-    $(".projects").on("click", openProjects);
-    $(".teammates").on("click", openTeamMates);  
-}
 
-function openClients(e) {
-	e.preventDefault();
-	$(".clients-content-box").removeClass("hidden");
-	$(".contacts-content-box").addClass("hidden");
-	$(".projects-content-box").addClass("hidden");
-	$(".team-mates-content-box").addClass("hidden");
-	$(".calendar-content-box").addClass("hidden");
-}
-
-// for the Contact page
-function openContacts(e) {
-	e.preventDefault();
-	$(".contacts-content-box").removeClass("hidden");
-	$(".projects-content-box").addClass("hidden");
-	$(".clients-content-box").addClass("hidden");
-	$(".team-mates-content-box").addClass("hidden");
-	$(".calendar-content-box").addClass("hidden");
-	attachOpenContactsforCompany()
-}
-
-function attachOpenContactsforCompany() {
-	$(".company-name").on("click", filterContactList);
-}
-
-function filterContactList(e){
-	let dynamicClassCompanyName = e.target.className.split(' ')[0]
-	e.preventDefault();
-	// remove the hidden class for the names that are from this company
-	$(`.${dynamicClassCompanyName}-contact`).removeClass("hidden");
-	attachHideFilteredContactsforCompany()
-}
-
-function attachHideFilteredContactsforCompany(){
-	$(".company-name").on("click", hideContactList);
-}
-
-function hideContactList(e){
-	let dynamicClassCompanyName = e.target.className.split(' ')[0]
-	e.preventDefault();
-	// add the hidden class for the names that are from this company
-	$(`.${dynamicClassCompanyName}-contact`).addClass("hidden");
-	attachOpenContactsforCompany()
-}
-
-// For the Team Mates page
-function openTeamMates(e) {
-	e.preventDefault();
-	$(".team-mates-content-box").removeClass("hidden");
-	$(".contacts-content-box").addClass("hidden");
-	$(".projects-content-box").addClass("hidden");
-	$(".clients-content-box").addClass("hidden");
-	$(".calendar-content-box").addClass("hidden");
-}
-
-function openProjects(e) {
-	e.preventDefault();
-	$(".projects-content-box").removeClass("hidden");
-	$(".contacts-content-box").addClass("hidden");
-	$(".clients-content-box").addClass("hidden");
-	$(".team-mates-content-box").addClass("hidden");
-	$(".calendar-content-box").addClass("hidden");
-}
 
 
 
