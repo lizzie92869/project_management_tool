19c738f2d7f9ea88a4efb01ed8484fa770f77795
Lizzie <lizzie.sorin@hotmail.fr>
2018-10-24 16:34:40 -0700

    test ignore .env

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -21,6 +21,15 @@ function openClients(e) {
 	$(".calendar-content-box").addClass("hidden");
 }
 
+function myMap() {
+var mapProp= {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+};
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+
+
 // for the Contact page
 function openContacts(e) {
 	e.preventDefault();
